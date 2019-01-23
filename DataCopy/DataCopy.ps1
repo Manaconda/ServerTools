b6f7c2d9beabc2deab61da09c054028582ad8c4f
Andy Taylor <andy0taylor@gmail.com>
2019-01-23 10:18:59 +0000

    Catching Up

--- DataCopy/DataCopy.ps1
+++ DataCopy/DataCopy.ps1
@@ -2,6 +2,6 @@ $drive=Read-Host "Source Drive"
 $destination=Read-Host "Destination"
 while ($TRUE){
     $source=Read-Host "Directory"
-    $arg=""""+$drive+"\"+$source+""""+" "+""""+$destination+"\"+$source+""""+" /e /r:0 /w:0 /mt"
+    $arg=""""+$drive+"\"+$source+""""+" "+""""+$destination+"\"+$source+""""+" /e /r:0 /w:0 /mt /log:$source.txt"
     Start-Process "robocopy.exe" -ArgumentList $arg
 }
--- ServerBuild/GuestConfig.ps1
+++ ServerBuild/GuestConfig.ps1
@@ -1,9 +1,3 @@
-# rdp
-# firewall off
-# ie enhanced
-
-
-
 Install-WindowsFeature AD-Domain-Services -IncludeAllSubFeature -IncludeManagementTools -Confirm
 Install-WindowsFeature DHCP -IncludeAllSubFeature -IncludeManagementTools -Confirm
 Install-WindowsFeature Print-Server -IncludeAllSubFeature -IncludeManagementTools -Confirm
@@ -12,4 +6,11 @@ Install-WindowsFeature WDS -IncludeAllSubFeature -IncludeManagementTools -Confir
 Install-WindowsFeature RSAT-Feature-Tools-BitLocker -IncludeAllSubFeature -IncludeManagementTools -Confirm
 Install-WindowsFeature Telnet-Client -IncludeAllSubFeature -IncludeManagementTools -Confirm
 Install-WindowsFeature Search-Service -IncludeAllSubFeature -IncludeManagementTools -Confirm
+
+netsh firewall set opmode mode = DISABLE profile = ALL
+
+
+$AdminKey = “HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}”
+Set-ItemProperty -Path $AdminKey -Name “IsInstalled” -Value 0
+Stop-Process -Name Explorer
 
