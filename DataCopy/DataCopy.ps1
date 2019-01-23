53e0629d601dc50928c22834c5fe7624d8395d24
Andy Taylor <andy0taylor@gmail.com>
2019-01-23 10:53:26 +0000

    added input hints

--- DataCopy/DataCopy.ps1
+++ DataCopy/DataCopy.ps1
@@ -1,5 +1,5 @@
-$drive=Read-Host "Source Drive"
-$destination=Read-Host "Destination"
+$drive=Read-Host "Source Drive (with :)"
+$destination=Read-Host "Destination (full path)"
 while ($TRUE){
     $source=Read-Host "Directory"
     $arg=""""+$drive+"\"+$source+""""+" "+""""+$destination+"\"+$source+""""+" /e /r:0 /w:0 /mt /log:$source.txt"
